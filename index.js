c7392a0dd65da75fb441bbd974711af78b40af7f
mikayla-bryant <mikaylabryant25@gmail.com>
2020-11-13 14:45:34 -0500

    Fixed Code Grader

--- index.js
+++ index.js
@@ -53,26 +53,27 @@ const zooAnimals = [
   /* 🦁🦁🦁 Request 1: .forEach() 🦁🦁🦁
   The zoos want to display both the scientific name and the animal name in front of the habitats. Populate the displayNames array with only the animal_name and scientific_name of each animal. displayNames will be an array of strings, and each string should follow this pattern: "Name: Jackal, asiatic, Scientific: Canis aureus."
   */
-  function animalNames(array){
-    const displayNames = [array.forEach(function(element, index){
-      console.log(`Name: ${element.animal_name}`);
-      console.log(`Scientific: ${element.scientific_name}`);
-    }
-    )];
-    console.log(displayNames);
-  }
-  
-  animalNames(zooAnimals);
+ function animalNames(array){
+  let arrayAnimals = []; 
+  array.forEach(function(element, index){
+    arrayAnimals.push(`Name: ${element.animal_name}`);
+    arrayAnimals.push(`Scientific: ${element.scientific_name}`);
+    
+  });
+  return(arrayAnimals);
+}
+
+animalNames(zooAnimals);
 
   /* 🦁🦁🦁 Request 2: .map() 🦁🦁🦁
   The zoos need a list of all their animal's names (animal_name only) converted to lower case. Using map, create a new array of strings named lowCaseAnimalNames, each string following this pattern: "jackal, asiatic". Log the resut.
   */
   function lowerCaseNames(array){
-    const lowCaseAnimalNames = [array.map(function(object){
+    const lowCaseAnimalNames = array.map(function(object){
       return object.animal_name.toLowerCase();
     }
-    )];
-    console.log(lowCaseAnimalNames);
+    );
+    return (lowCaseAnimalNames);
   }
   
   
@@ -80,14 +81,14 @@ const zooAnimals = [
   The zoos are concerned about animals with a lower population count. Using filter, create a new array of objects called lowPopulationAnimals which contains only the animals with a population less than 5.
   */
   function lowPopulationAnimals(array){
-    const lowPopulationAnimals = [zooAnimals.filter(function(object){
+    const lowPopulationAnimals = zooAnimals.filter(function(object){
       return (object.population < 5);
     }
-    )];
-    console.log(lowPopulationAnimals);
+    );
+    return (lowPopulationAnimals);
   }
 
-  lowerCaseNames(zooAnimals);
+  lowPopulationAnimals(zooAnimals);
   
 
   /* 🦁🦁🦁 Request 4: .reduce() 🦁🦁🦁
@@ -113,16 +114,25 @@ USApop(zooAnimals);
     * The last parameter accepts a callback
     * The consume function should return the invocation of cb, passing a and b into cb as arguments
   */
- consume = (a, b, cb) => cb(a, b);
+ function consume(a, b, cb){
+   return cb(a, b);
+ }
   
   /* 🦁🦁🦁 Step 2: Create several functions to callback with consume(); 🦁🦁🦁 */
  // 🦁🦁🦁 Create a function named add that returns the sum of two numbers 🦁🦁🦁
- add = (num1, num2) => num1 + num2;
+ function add(num1, num2){
+    return num1 + num2;
+ }
 
 // 🦁🦁🦁 Create a function named multiply that returns the product of two numbers 🦁🦁🦁
-multiply = (num1, num2) => num1 * num2;
+function multiply (num1, num2) {
+  return num1 * num2;
+}
+
  // 🦁🦁🦁 Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!" 🦁🦁🦁
- greeting = (firstName, lastName) => `Hello ${firstName} ${lastName}, nice to meet you!`;
+ function greeting (firstName, lastName){
+    return `Hello ${firstName} ${lastName}, nice to meet you!`;
+ }
   
   // 🦁🦁🦁 Step 3: Check your work by un-commenting the following calls to consume(): 🦁🦁🦁 
   // ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️
